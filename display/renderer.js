a2a757b9e190e9489eb57bdbac5a615e6446f178
Shivank Vatsal <shivankvatsal@gmail.com>
2022-03-11 20:21:32 -0500

    simulated icon & direction, handling sounds on axes pending

--- display/renderer.js
+++ display/renderer.js
@@ -14,6 +14,37 @@ var quadr;
 var sect; 
 var mult; 
 var mult2; 
+var randImg;
+
+let Computer_keyboard; 
+let Cupboard_open_or_close; 
+let Drawer_open_or_close; 
+let Female_speech_and_woman_speaking; 
+let Finger_snapping;
+let Keys_jangling;
+let Knock;
+let Laughter; 
+let Male_speech_and_man_speaking; 
+let Printer;
+let Scissors;
+let Telephone;
+let Writing;
+
+function preload() {
+  Computer_keyboard = loadImage('icons/keyboard.png');
+  Cupboard_open_or_close = loadImage('icons/door-open.png');
+  Drawer_open_or_close = loadImage('icons/drawer.png');
+  Female_speech_and_woman_speaking = loadImage('icons/femchat.png');
+  Finger_snapping = loadImage('icons/snap.png');
+  Keys_jangling = loadImage('icons/key-chain.png');
+  Knock = loadImage('icons/knock.png');
+  Laughter = loadImage('icons/laugh.png');
+  Male_speech_and_man_speaking = loadImage('icons/malechat.png');
+  Printer = loadImage('icons/printer.png');
+  Scissors = loadImage('icons/scissors.png');
+  Telephone = loadImage('icons/phone.png');
+  Writing = loadImage('icons/writing.png');
+}
 
 // Called once.
 function setup() {
@@ -22,36 +53,37 @@ function setup() {
 	theta = 0;
   //x = 100
   //y = 100
+  iconsz = 40; //adjust icon size
   dvsn = 32; //number of segments in arc
   start = 0;
   end = PI/(dvsn/2);
-  
+  imgArr = ["Computer_keyboard", "Cupboard_open_or_close", "Drawer_open_or_close", "Female_speech_and_woman_speaking", 
+    "Finger_snapping", "Keys_jangling", "Knock", "Laughter", "Male_speech_and_man_speaking", "Printer", "Scissors", "Telephone",
+    "Writing"];
   // // Init values.
   // radius = windowHeight * 0.45
   // angle = 0
   // angle_vel = 0.01
-
- 
-
-  noFill();
-  
+  noFill();  
   strokeWeight(5);
   frameRate(1);
-  
 }
 
 // Called periodically.
 function draw() {
   clear() 
   noFill();
+  drawingContext.shadowBlur = 0;
   background('rgba(255, 255, 255, 0.1)')
-  strokeWeight(5);
   mult = Math.random() < 0.5 ? -1 : 1;
   mult2 = Math.random() < 0.5 ? -1 : 1;
   num = Math.random();
   num2 = Math.random();
+  randImg = imgArr[Math.floor(Math.random() * 13)];
+  let sect = getSector(mult * num, mult2 * num2);
   for(let i = 0; i < dvsn; i++){
-    if(i == getSector(mult * num, mult2 * num2)){
+    if(i == sect){
+      image(eval(randImg), iconX(sect) - (iconsz/2), iconY(sect) - (iconsz/2), iconsz, iconsz);
       stroke(255);
       drawingContext.shadowBlur = 20;
       drawingContext.shadowColor = color(207, 7, 70);
@@ -88,8 +120,6 @@ function draw() {
   // }  
 }
 
-
-
 // ref: https://p5js.org/reference/#/p5.Vector/magSq
 function drawArrow(base, vec, myColor) {
   push();
@@ -114,6 +144,18 @@ function dispIcon(x, y){
     point(x, y);
   }
 }
+
+function iconX(sector){
+  sector = dvsn - sector; 
+  let deg = (180/dvsn) + ((360/dvsn) * (sector - 1));
+  return (windowWidth/2 + (250 * Math.cos(deg * PI/180)));
+}
+
+function iconY(sector){
+  sector = dvsn - sector;
+  let deg = (180/dvsn) + ((360/dvsn) * (sector - 1));
+  return (windowHeight/2 - (250 * Math.sin(deg * PI/180)));
+ }
 
 function getSector(x, y){
   angle = Math.abs((180/PI) * Math.atan(y/x));
