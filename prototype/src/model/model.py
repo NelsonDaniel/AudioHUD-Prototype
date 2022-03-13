dfbd32e804590670a55b47a5ac489aa8a4a63baf
NelsonDaniel <nd.ta@hotmail.com>
2022-03-13 18:42:10 -0400

    prototype: python executable to javascript working

--- prototype/src/model/model.py
+++ prototype/src/model/model.py
@@ -1,14 +1,30 @@
 import os
 import csv
 import time
+import zmq
+import json
+import sys
 
-with open(os.path.join(os.path.dirname(__file__),'sample-detections.csv'), newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        time.sleep(0.1)
-        c = row['class']
-        x = row['x']
-        y = row['y']
-        z = row['z']
-        if c != 'NOTHING':
-            print(c, x, y, z)
+os.chdir(sys._MEIPASS)
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    with open('sample-detections.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                message = socket.recv()
+                print(f"Received request: {message}")
+                time.sleep(0.1)
+                c = row['class']
+                x = row['x']
+                y = row['y']
+                z = row['z']
+                result = [c,x,y,z]
+                json_string = json.dumps(result)
+                socket.send_string(json_string)
+
+        
+
+
