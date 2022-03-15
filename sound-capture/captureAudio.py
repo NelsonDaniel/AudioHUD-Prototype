104254418c20301044cf3f7647203915b5354a5b
eliasHw <76268791+eliasHw@users.noreply.github.com>
2022-03-15 00:47:28 -0400

    Auto selects default audio device; minor additions

--- sound-capture/captureAudio.py
+++ sound-capture/captureAudio.py
@@ -1,12 +1,13 @@
-import numpy
+from msilib.schema import Error
+import numpy as np
 import pyaudio
 import wave
-import soundfile as sf
 import librosa
-from matplotlib import pylab
 import io
+from scipy.signal import stft
 
 # Note: python 3.7 needs to used with this wheel of pyaudio: https://github.com/intxcc/pyaudio_portaudio
+# Currently only works on Windows
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
@@ -20,12 +21,19 @@ chunk = 4410 # Record in chunks of 1024 samples; Use 4410 because fs/chunk = 10 
 # Returns a valid device ID 
 # Need to verify deviceID is valid
 def getAudioDevice():
-    print ( "Available devices:\n")
+
+    if p.get_default_host_api_info()['name'] == 'Windows WASAPI':
+        return p.get_default_output_device_info()
+
+    default_device_name = p.get_default_output_device_info()['name']
     for i in range(0, p.get_device_count()):
-        info = p.get_device_info_by_index(i) 
-        print ( str(info["index"]) +  ": \t %s \n \t %s \n" % (info["name"], p.get_host_api_info_by_index(info["hostApi"])["name"]))
-        pass
-    return int(input("Enter device ID: "))
+        info = p.get_device_info_by_index(i)
+        if p.get_host_api_info_by_index(info["hostApi"])["name"] == 'Windows WASAPI':
+            if p.get_default_output_device_info()['name'] in info['name']:
+                return info
+    
+    raise ValueError("Error selecting audio device.") 
+
 
 
 # Returns a BytesIO object that stored all the frames
@@ -40,11 +48,40 @@ def save_data(frames):
 
     audioData.seek(0)
     return audioData
+
+def spectrum_fast(x, nperseg=512, noverlap=384, window='hamming', cut_dc=True,
+                  output_phase=True, cut_last_timeframe=True):
+    '''
+    Compute magnitude spectra from monophonic signal
+    '''
+
+    f, t, seg_stft = stft(x,
+                        window=window,
+                        nperseg=nperseg,
+                        noverlap=noverlap)
+
+    #seg_stft = librosa.stft(x, n_fft=nparseg, hop_length=noverlap)
+
+    output = np.abs(seg_stft)
+
+    if output_phase:
+        phase = np.angle(seg_stft)
+        output = np.concatenate((output,phase), axis=-3)
+
+    if cut_dc:
+        output = output[:,1:,:]
+
+    if cut_last_timeframe:
+        output = output[:,:,:-1]
+
+    #return np.rot90(np.abs(seg_stft))
+    return output
     
 
-device_id = getAudioDevice()
 
-device_info = p.get_device_info_by_index(device_id)
+device_info = getAudioDevice()
+device_id = device_info["index"]
+
 channels = device_info["maxInputChannels"] if (device_info["maxOutputChannels"] < device_info["maxInputChannels"]) else device_info["maxOutputChannels"]
 sampleRate = int(device_info["defaultSampleRate"])
 
@@ -60,13 +97,14 @@ stream = p.open(format=sample_format,
                 )
 
 
-print('\nRecording', device_id, '...\n')
+seconds = 5 # Number of seconds you would like to capture audio
+print('\nRecording from', device_info['name'], 'for ', seconds, 'seconds...\n')
 
 frames = []  # Initialize array to store frames
 num_frames = 0
 max_frames = 10
 
-while 1:
+while seconds>0:
     data = stream.read(chunk)   
     frames.append(data)
     num_frames += 1
@@ -75,10 +113,14 @@ while 1:
         wav_data = save_data(frames)
 
         x_byteio, sr = librosa.load(wav_data, sr=32000, mono=False)
-        print(x_byteio, '\n')
+        spf = spectrum_fast(x_byteio)
+        print(spf.shape)
+        # print("\n=====\n")
+        # print(x_byteio, '\n')
 
         frames = []
         num_frames = 0
+        seconds -= 1
 
 # Maybe need to handle last chunk/partial chunk?
 # For now, Cmd+C to stop program
