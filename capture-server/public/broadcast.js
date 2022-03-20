9604a42ff8712e080329934ee1b267a5ba986ed3
NelsonDaniel <nd.ta@hotmail.com>
2022-03-20 13:30:15 -0400

    fixed server

---
+++ capture-server/public/broadcast.js
@@ -0,0 +1,112 @@
+const peerConnections = {};
+const config = {
+  iceServers: [
+    { 
+      "urls": "stun:stun.l.google.com:19302",
+    },
+    // { 
+    //   "urls": "turn:TURN_IP?transport=tcp",
+    //   "username": "TURN_USERNAME",
+    //   "credential": "TURN_CREDENTIALS"
+    // }
+  ]
+};
+
+const socket = io.connect(window.location.origin);
+
+socket.on("answer", (id, description) => {
+  peerConnections[id].setRemoteDescription(description);
+});
+
+socket.on("watcher", id => {
+  const peerConnection = new RTCPeerConnection(config);
+  peerConnections[id] = peerConnection;
+
+  let stream = videoElement.srcObject;
+  stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+  peerConnection.onicecandidate = event => {
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
+    }
+  };
+
+  peerConnection
+    .createOffer()
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(() => {
+      socket.emit("offer", id, peerConnection.localDescription);
+    });
+});
+
+socket.on("candidate", (id, candidate) => {
+  peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+});
+
+socket.on("disconnectPeer", id => {
+  peerConnections[id].close();
+  delete peerConnections[id];
+});
+
+window.onunload = window.onbeforeunload = () => {
+  socket.close();
+};
+
+const videoElement = document.querySelector("video");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const videoSelect = document.getElementById("videoSelectBtn");
+const audioSelect = document.querySelector("select#audioSource");
+
+videoSelect.onclick = getStream;
+
+function getDevices() {
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function gotDevices(deviceInfos) {
+  window.deviceInfos = deviceInfos;
+  for (const deviceInfo of deviceInfos) {
+    const option = document.createElement("option");
+    option.value = deviceInfo.deviceId;
+    option.text = deviceInfo.label;
+    audioSelect.appendChild(option);
+  }
+}
+
+
+async function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+
+  const stream = await navigator.mediaDevices.getDisplayMedia(
+    {
+      video: true,
+      audio: {
+        noiseSuppression: true,
+        channelCount: 2,
+        sampleRate
+      }
+    }
+  );
+
+  videoElement.srcObject = stream;
+  socket.emit("broadcaster");
+}
+
+// start button
+startBtn.onclick = e => {
+  mediaRecorder.start();
+  startBtn.classList.add('is-danger');
+  startBtn.innerText = 'Recording';
+};
+
+// stop button
+stopBtn.onclick = e => {
+  mediaRecorder.stop();
+  startBtn.classList.remove('is-danger');
+  startBtn.innerText = 'Start';
+};
